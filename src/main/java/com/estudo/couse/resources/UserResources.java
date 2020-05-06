f23dec30308b37e8be5b9eb9bf8137becb6ddcf3
gustavojordaob <gustavojordaob@outlook.com>
2020-05-06 12:35:09 -0300

    criando o service

--- src/main/java/com/estudo/couse/resources/UserResources.java
+++ src/main/java/com/estudo/couse/resources/UserResources.java
@@ -1,19 +1,33 @@
 package com.estudo.couse.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.estudo.couse.entities.User;
+import com.estudo.couse.services.UserService;
 
 @RestController
 @RequestMapping(value =	"/users")
 public class UserResources {
 	
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User user = new User(1L, "Maria", "maria@gmail.com","999999999","123123");
-		return ResponseEntity.ok().body(user);
+	public ResponseEntity<List<User>> findAll(){
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+											
+	@GetMapping("/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
---
+++ src/main/java/com/estudo/couse/services/UserService.java
@@ -0,0 +1,26 @@
+package com.estudo.couse.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.estudo.couse.entities.User;
+import com.estudo.couse.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	public List<User> findAll(){
+		return userRepository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = userRepository.findById(id);
+		return obj.get();
+	}
+}
